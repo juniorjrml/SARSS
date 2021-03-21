25c42525fa22ff635a76f6c01dd41a60c425c365
juniorjrml <jancarlolcj@gmail.com>
2021-03-21 18:23:52 -0300

    adaptando o projeto para rodar no heroku.com

--- web_api_SARSS/web_api_SARSS/settings.py
+++ web_api_SARSS/web_api_SARSS/settings.py
@@ -138,3 +138,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR,'static')# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
