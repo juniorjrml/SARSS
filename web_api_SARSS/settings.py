7e33edb437bcb3efa6019828718290672ad75c38
juniorjrml <jancarlolcj@gmail.com>
2021-03-24 18:38:25 -0300

    corrigindo algumas funcionalidades em procurar usuarios (alterando arquivo visualizar_usuario.html) e settings pra resolver probelma com static

--- web_api_SARSS/settings.py
+++ web_api_SARSS/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'static')# location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'/static/')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
